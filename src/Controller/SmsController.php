94cec5ea7f4e5bf1acd6891e5781e14adec7afbe
Habibun Noby <habibun.noby@gmail.com>
2022-03-05 15:30:51 +0600

    feat: fix sns config

--- src/Controller/SmsController.php
+++ src/Controller/SmsController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Notifier\Message\SmsMessage;
 use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Notifier\Recipient\Recipient;
+use Symfony\Component\Notifier\TexterInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SmsController extends AbstractController
@@ -23,5 +25,23 @@ class SmsController extends AbstractController
 
         // send the notification to the recipient
         $notifier->send($notification, $recipient);
+
+        exit('sent');
+    }
+
+    #[Route('/send-sms', name: 'send_app_sms')]
+    public function sendSms(TexterInterface $texter)
+    {
+//        dd($_ENV['AMAZON_SNS_DSN']);
+        $sms = new SmsMessage(
+        // the phone number to send the SMS message to
+            '+8801724595756',
+            // the message
+            'A new login was detected!'
+        );
+
+        $texter->send($sms);
+
+        exit('sent');
     }
 }
